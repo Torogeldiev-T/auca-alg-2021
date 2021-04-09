af4ec546367a5c4df2f0e9a637b647ce81915ad4
Torogeldiev-T <tilek2402@gmail.com>
2021-04-09 13:36:13 +0600

    uva-10887wrong answer

---
+++ project-01/uva-10887/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+int main()
+{
+    int cases;
+    cin >> cases;
+    for (int i = 0; i < cases; i++)
+    {
+        int m, n;
+        vector<string> s1, s2;
+        cin >> m >> n;
+        cin.clear();
+        cin.ignore();
+        for (int j = 0; j < m; j++)
+        {
+            string s;
+            getline(cin, s);
+            s1.push_back(s);
+        }
+        for (int j = 0; j < n; j++)
+        {
+            string s;
+            getline(cin, s);
+            s2.push_back(s);
+        }
+        unordered_set<string> res_set;
+        int res = 0;
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                if (s1[j].empty())
+                    s1[j] = "-";
+                if (s2[k].empty())
+                    s2[k] = "+";
+
+                string temp = s1[j] + s2[k];
+                if (res_set.insert(temp).second)
+                    res++;
+            }
+        }
+        // for (auto j = res_set.begin(); j != res_set.end(); j++)
+        // {
+        //     cout << *j << "\n";
+        // }
+        cout << "Case " << i + 1 << ": " << res << "\n";
+    }
+}
